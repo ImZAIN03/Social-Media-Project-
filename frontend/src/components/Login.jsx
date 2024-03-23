205de57ddf89ccbf0724ed717025aa8156c93e8d
Muhammed Zain <muhammedzain2256@gmail.com>
2024-03-24 02:40:58 +0500

    General Frontend UI Done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,13 +11,13 @@ const Login = () => {
     <div className='w-screen h-screen flex items-center justify-center'>
       <div className='flex items-center justify-evenly w-[80%]'>
         <div>
-          <img className='ml-1' width={"180px"} src='SM.png' alt='Logo'/>
+          <img className='ml-1' width={"300px"} src='SM.png' alt='Logo'/>
         </div>
       <div >
         <div className='my-5'>
           <h1 className='font-bold text-6xl'>Happening Now.</h1>
         </div>
-        <h1 className='mt-4 mb-2 text-2xl font-bold'>Login</h1>
+        <h1 className='mt-4 mb-2 text-2xl font-bold'>{isLogin ? "Login" : "Sign Up"}</h1>
         <form className='flex flex-col w-[70%]'>
           {
             !isLogin && (<>
@@ -27,8 +27,8 @@ const Login = () => {
           }
           <input type='text' placeholder='Email' className='bg-transparent outline-sky-600 border-2 border-sky-600 px-3 py-1 rounded-full my-1 font-semibold'/>
           <input type='password' placeholder='Password' className='bg-transparent outline-sky-600 border-2 border-sky-600 px-3 py-1 rounded-full my-1 font-semibold'/>
-          <button className='px-4 py-2 text-lg border-none bg-cyan-600 text-white rounded-full my-3'>Login</button>
-          <h1>{isLogin ? "Don't have and account?" :  "Already have and account?"} <span className='cursor-pointer' onClick={loginSignupHandler}>{isLogin ? "Register" :"Login"}</span></h1>
+          <button className='px-4 py-2 text-lg border-none bg-cyan-600 text-white rounded-full my-3'>{isLogin ? "Login" : "Create Account"}</button>
+          <h1 className='font-semibold'>{isLogin ? "Don't have and account?" :  "Already have and account?"} <span className='cursor-pointer font-semibold text-sky-600 text-lg' onClick={loginSignupHandler}>{isLogin ? "Sign Up" :"Login"}</span></h1>
         </form>
       </div>
       </div>
