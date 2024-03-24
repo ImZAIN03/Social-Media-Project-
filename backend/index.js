4b912d9a6bd83bdee9032370c3f9273dc391dfca
Muhammed Zain <muhammedzain2256@gmail.com>
2024-03-25 03:20:11 +0500

    Backend Work

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import databaseConnection from './config/database.js';
+import cookieParser from 'cookie-parser';
+import userRoute from './routes/userRoute.js'
+
 dotenv.config({
     path: '.env'
 })
 databaseConnection();
 const app = express();
+
+//middleware
+app.use(express.urlencoded({
+    extends: true
+}));
+app.use(express.json())
+app.use(cookieParser())
+
+// api
+app.use("/api/v1/user", userRoute)
+
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running on Port ${process.env.PORT}`);
